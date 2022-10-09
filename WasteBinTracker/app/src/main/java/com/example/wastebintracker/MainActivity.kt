93a14ccf02be1a36f571ac8cd28c1fb89bdc478f
M-J-V <malcolmvas@gmail.com>
2022-10-09 23:43:05 +0200

    Initial app

---
+++ WasteBinTracker/app/src/main/java/com/example/wastebintracker/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.wastebintracker
+
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.wastebintracker.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_weight_data, R.id.navigation_goals
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+        var autoDate = findViewById<TextView>(R.id.autoDate)
+
+        val c = Calendar.getInstance()
+
+        val year = c.get(Calendar.YEAR)
+        //val strYear = year as String
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        val curDate = year
+        //autoDate.text = curDate
+    }
+}
